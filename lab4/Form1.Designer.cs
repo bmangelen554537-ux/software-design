2eab51d432c3008c653e3617cf6d1387d488f138
bmangelen554537-ux <b.mangelen.554537@umindanao.edu.ph>
2026-04-18 22:30:58 +0800

    done lab4

--- lab4/Form1.Designer.cs
+++ lab4/Form1.Designer.cs
@@ -20,20 +20,55 @@
 			base.Dispose(disposing);
 		}
 
-		#region Windows Form Designer generated code
+        #region Windows Form Designer generated code
 
-		/// <summary>
-		///  Required method for Designer support - do not modify
-		///  the contents of this method with the code editor.
-		/// </summary>
-		private void InitializeComponent()
-		{
-			this.components = new System.ComponentModel.Container();
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "Form1";
-		}
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            listBox1 = new ListBox();
+            btnTest = new Button();
+            SuspendLayout();
+            // 
+            // listBox1
+            // 
+            listBox1.BackColor = SystemColors.Info;
+            listBox1.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 30;
+            listBox1.Location = new Point(12, 21);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(776, 364);
+            listBox1.TabIndex = 0;
+            // 
+            // btnTest
+            // 
+            btnTest.Font = new Font("Segoe UI", 24F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnTest.Location = new Point(12, 391);
+            btnTest.Name = "btnTest";
+            btnTest.Size = new Size(776, 53);
+            btnTest.TabIndex = 1;
+            btnTest.Text = "Test";
+            btnTest.UseVisualStyleBackColor = true;
+            btnTest.Click += btnTest_Click_1;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnTest);
+            Controls.Add(listBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
 
-		#endregion
-	}
+        #endregion
+
+        private ListBox listBox1;
+        private Button btnTest;
+    }
 }
